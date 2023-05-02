6c166eb9d648adbcefb91634ebbd347885ae3a79
tommasobaccano@gmail.com <tommasobaccano@gmail.com>
2023-05-02 17:27:31 +0200

    esercizi Razor

---
+++ la-mia-pizzeria-razor-layout/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(string img, string name, string description, double price)
+        {
+            Img = img;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public string Img { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
